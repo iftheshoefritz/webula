880a67d7ec68f1ab18f816f1f0a5be2ad809169c
Fritz Meissner <fritz.meissner@gmail.com>
2023-07-18 16:58:06 +0100

    Forgot to commit constants

---
+++ src/lib/constants.js
@@ -0,0 +1,9 @@
+export const textColumns = [
+  'name', 'set', 'rarity', 'unique', 'collectorsinfo', 'type', 'mission', 'dilemmatype',
+  'quadrant', 'affiliation', 'icons', 'staff', 'keywords', 'class', 'species', 'skills',
+  'gametext'
+];
+
+export const rangeColumns = [
+  'cost', 'span', 'points', 'integrity', 'range', 'cunning', 'weapons', 'strength', 'shields'
+];
