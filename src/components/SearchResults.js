ad95b4061c752ede561db0eabd420f3d0d79f78f
Fritz Meissner <fritz.meissner@gmail.com>
2023-07-25 09:39:20 +0100

    Fix missing key build error

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,23 +4,22 @@ export default function SearchResults({ filteredData, onCardSelected, currentDec
   return (
     <>
       {filteredData.map((row, index) => (
-        <div className="relative">
-          <Image
-            src={`/cardimages/${row.imagefile}.jpg`}
-            width={165}
-            height={229}
-            placeholder='blur'
-            blurDataURL='/cardimages/cardback.jpg'
-            alt={row.name}
-            key={index}
-            className='w-full h-auto'
-            onClick={() => (onCardSelected(row))}
-          />
-          { currentDeck &&
-          <div className="absolute top-0 right-0 bg-black bg-opacity-50 text-white rounded-full px-2 py-1">
-            { currentDeck[row.collectorsinfo]?.row?.count || 0}
-          </div>
-          }
+          <div className="relative" key={index}>
+            <Image
+              src={`/cardimages/${row.imagefile}.jpg`}
+              width={165}
+              height={229}
+              placeholder='blur'
+              blurDataURL='/cardimages/cardback.jpg'
+              alt={row.name}
+              className='w-full h-auto'
+              onClick={() => (onCardSelected(row))}
+            />
+            { currentDeck &&
+            <div className="absolute top-0 right-0 bg-black bg-opacity-50 text-white rounded-full px-2 py-1">
+              { currentDeck[row.collectorsinfo]?.row?.count || 0}
+            </div>
+            }
         </div>
       ))}
     </>
