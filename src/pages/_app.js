6e9c7d9b36ed9491de3ad4bf462c16acbc20588c
Fritz Meissner <fritz.meissner@gmail.com>
2023-10-09 08:05:44 +0100

    Revert "Move analytics into _app.js"
    This reverts commit e3a68f464067651e4aa16726933eb082e53fe1f2 because it resulted
    in the app just showing a blank page. Reverting until I understand the analytics
    process better.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,5 @@
-import { Analytics } from '@vercel/analytics/react';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <Analytics>
-      <Component {...pageProps} />
-    </Analytics>
-  )
+  return <Component {...pageProps} />
 }
