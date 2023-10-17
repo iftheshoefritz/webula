14252eb29cff03f7a853fcc3c3cdb99eed8d057e
Fritz Meissner <fritz.meissner@gmail.com>
2023-10-17 15:16:57 +0100

    Add example of negative search term

--- src/components/Help.js
+++ src/components/Help.js
@@ -7,18 +7,17 @@ export default function Help() {
         <div className="flex flex-wrap max-h-0 overflow-y-scroll peer-checked:max-h-80">
           <div className="my-2">
             <p className="font-bold">Example searches:</p>
-            <p>
-              <ul className="list-disc">
-                <li>name:Odo</li>
-                <li>strength:8-10</li>
-                <li>strength:8-8 (exactly 8)</li>
-                <li>gametext:&quot;while facing&quot;</li>
-                <li>type:personnel text:selected</li>
-                <li>keywords:drone icons:sta</li>
-                <li>keywords:legate keywords:dissident</li>
-                <li>skills:&quot;2 astrometrics&quot;</li>
-              </ul>
-            </p>
+            <ul className="list-disc">
+              <li>name:Odo</li>
+              <li>strength:8-10</li>
+              <li>strength:8-8 (exactly 8)</li>
+              <li>gametext:&quot;while facing&quot;</li>
+              <li>type:personnel text:selected</li>
+              <li>keywords:drone icons:sta</li>
+              <li>keywords:legate keywords:dissident</li>
+              <li>skills:&quot;2 astrometrics&quot;</li>
+              <li>-skills:acquisition (personnel without acquisition)</li>
+            </ul>
           </div>
           <div>
             <p className="font-bold">Text fields, e.g. <i>name:odo</i></p>
