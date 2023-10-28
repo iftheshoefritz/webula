f52a2454490b663ae1176ab27274410c6e299655
Fritz Meissner <fritz.meissner@gmail.com>
2023-10-28 11:34:22 +0100

    Add abbreviations to search
    e.g. c = cost, int = integrity

--- src/hooks/useFilterData.js
+++ src/hooks/useFilterData.js
@@ -1,6 +1,6 @@
 import searchQueryParser from 'search-query-parser';
 import { useState, useEffect } from 'react';
-import { textColumns, rangeColumns } from '../lib/constants';
+import { textColumns, textAbbreviations, rangeColumns, rangeAbbreviations } from '../lib/constants';
 import { track } from '@vercel/analytics';
 
 const QUOTE_CHARS_REGEX = /[‘’“”«»\u2018\u2019\u201C\u201D]/g;
@@ -13,31 +13,39 @@ function toArray(item) {
   }
 }
 
+const colInQuery = (col, parsedQuery) =>
+      parsedQuery[col] ? col : (textAbbreviations[col] || rangeAbbreviations[col])
+
+
 const useFilterData = (loading, data, columns, searchQuery) => {
   console.log('starting useFilterData');
   const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
+    console.log(searchQuery);
     const parsedQuery = searchQueryParser.parse((searchQuery || '').replace(QUOTE_CHARS_REGEX, '"'), {
-      keywords: textColumns,
-      ranges: rangeColumns,
+      keywords: textColumns.concat(Object.values(textAbbreviations)),
+      ranges: rangeColumns.concat(Object.values(rangeAbbreviations)),
       offsets: false,
     });
+    console.log(parsedQuery);
     textColumns.forEach((column) => {
-      if (parsedQuery[column]) {
-        parsedQuery[column] = toArray(parsedQuery[column])
+      const fullOrAbbreviatedColumn = colInQuery(column, parsedQuery)
+      if (parsedQuery[fullOrAbbreviatedColumn]) {
+        parsedQuery[fullOrAbbreviatedColumn] = toArray(parsedQuery[fullOrAbbreviatedColumn])
           .map((term) => term.toLowerCase())
       }
-      if (parsedQuery.exclude && parsedQuery.exclude[column])
-        parsedQuery.exclude[column] = toArray(parsedQuery.exclude[column])
+      if (parsedQuery.exclude && parsedQuery.exclude[fullOrAbbreviatedColumn])
+        parsedQuery.exclude[fullOrAbbreviatedColumn] = toArray(parsedQuery.exclude[fullOrAbbreviatedColumn])
           .map((term) => term.toLowerCase())
     });
 
     const withoutExcluded = data.filter((row) => {
-      return columns.every((column) => {
-        if ( parsedQuery.exclude && parsedQuery.exclude[column] ) {
+      return textColumns.concat(rangeColumns).every((column) => {
+        const fullOrAbbreviatedColumn = colInQuery(column, parsedQuery)
+        if ( parsedQuery.exclude && parsedQuery.exclude[fullOrAbbreviatedColumn] ) {
           if (textColumns.includes(column)) {
-            return parsedQuery.exclude[column].every((match) =>
+            return parsedQuery.exclude[fullOrAbbreviatedColumn].every((match) =>
               !row[column].includes(match)
             )
           }
@@ -47,16 +55,17 @@ const useFilterData = (loading, data, columns, searchQuery) => {
     })
 
     const filtered = withoutExcluded.filter((row) => {
-      return columns.every((column) => {
-        if (parsedQuery[column]) {
+      return textColumns.concat(rangeColumns).every((column) => {
+        const fullOrAbbreviatedColumn = colInQuery(column, parsedQuery)
+        if (parsedQuery[fullOrAbbreviatedColumn]) {
           if (textColumns.includes(column)) {
-            return parsedQuery[column].every((match) =>
+            return parsedQuery[fullOrAbbreviatedColumn].every((match) =>
               row[column].includes(match)
             )
           } else if (rangeColumns.includes(column)) {
-            const range = parsedQuery[column];
+            const range = parsedQuery[fullOrAbbreviatedColumn];
             const rowValue = parseFloat(row[column]);
-            const fromValue = range.from !== '' && range.from !== undefined ? parseFloat(range.from) : -Infinity;
+            const fromValue = (range.from !== '' && range.from !== undefined) ? parseFloat(range.from) : -Infinity;
             const toValue = range.to !== '' && range.to !== undefined ? parseFloat(range.to) : Infinity;
             return rowValue >= fromValue && rowValue <= toValue;
           }
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,6 +4,40 @@ export const textColumns = [
   'gametext', 'hof'
 ];
 
+export const textAbbreviations = {
+  'name': 'n',
+  'set': 'se',
+  'rarity': 'r',
+  'unique': 'u',
+  'collectorsinfo': 'ci',
+  'type': 'ty',
+  'mission': 'mt',
+  'dilemmatype': 'dt',
+  'quadrant': 'q',
+  'affiliation': 'a',
+  'icons': 'i',
+  'staff': 'st',
+  'keywords': 'k',
+  'class': 'cl',
+  'species': 'sp',
+  'skills': 'sk',
+  'gametext': 't',
+  'hof': 'hof'
+};
+
 export const rangeColumns = [
   'cost', 'span', 'points', 'integrity', 'range', 'cunning', 'weapons', 'strength', 'shields'
 ];
+
+
+export const rangeAbbreviations = {
+  'cost': 'c',
+  'span': 's',
+  'points': 'p',
+  'integrity': 'int',
+  'range': 'ra',
+  'cunning': 'cunn',
+  'weapons': 'wp',
+  'strength': 'str',
+  'shields': 'sh',
+};
