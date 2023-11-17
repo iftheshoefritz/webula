76543d4964a6f364d6b9d93a0fd5914331f2ae21
Fritz Meissner <fritz.meissner@gmail.com>
2023-11-17 17:40:17 +0000

    Convert DeckListPile to function component

--- src/components/DeckListPile.js
+++ src/components/DeckListPile.js
@@ -1,34 +1,31 @@
 import React, { Component } from 'react';
 import DeckListItem from '../components/DeckListItem';
 
-class DeckListPile extends Component {
-  render() {
-    const { pileName, cardsForPile, cardCounts, incrementIncluded, decrementIncluded, sortBy } = this.props;
-    const count = cardsForPile.reduce((sum, row) => sum + row.count, 0);
-    return (
-      <div>
-        <span className="font-semibold">{pileName} ({count})</span>
-        <ul>
-          {
-            cardsForPile
-              .sort(sortBy)
-              .map((row) => {
-              return <DeckListItem
-                key={row.collectorsinfo}
-                collectorsinfo={row.collectorsinfo}
-                decrementIncluded={(e) => decrementIncluded(e, row)}
-                incrementIncluded={(e) => incrementIncluded(row)}
-                count={row.count}
-                name={row.originalName}
-                imagefile={row.imagefile}
-                unique={row.unique == 'y'}
-              />
-            })
-          }
-        </ul>
-      </div>
-    )
-  }
+function DeckListPile({ pileName, cardsForPile, cardCounts, incrementIncluded, decrementIncluded, sortBy }) {
+  const count = cardsForPile.reduce((sum, row) => sum + row.count, 0);
+
+  return (
+    <div>
+      <span className="font-semibold">{pileName} ({count})</span>
+      <ul>
+        {cardsForPile
+         .sort(sortBy)
+         .map((row) => (
+           <DeckListItem
+             key={row.collectorsinfo}
+             collectorsinfo={row.collectorsinfo}
+             decrementIncluded={(e) => decrementIncluded(e, row)}
+             incrementIncluded={(e) => incrementIncluded(row)}
+             count={row.count}
+             name={row.originalName}
+             imagefile={row.imagefile}
+             unique={row.unique === 'y'}
+           />
+         ))
+        }
+      </ul>
+    </div>
+  );
 }
 
 export default DeckListPile;
