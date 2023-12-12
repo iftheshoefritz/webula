1e7fb0da0394306a689837e37aa1a1fb7ae24efa
Fritz Meissner <fritz.meissner@gmail.com>
2023-12-12 17:31:07 +0000

    Add api/auth and api/drive endpoints

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,49 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXTAUTH_CLIENTID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+      authorization: {
+        params: {
+          scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email',
+            'https://www.googleapis.com/auth/drive.appdata'
+          ].join(' '),
+        }
+      }
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET as string,
+  callbacks: {
+    async jwt({token, account }) {
+      //console.log('*****************JWT CALLBACK!!')
+      //console.log('token', token)
+      //console.log('account', account)
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account?.refreshToken;
+      }
+      return token;
+    },
+    async session(session, token, user) {
+      // Forward the access token to the session
+      //console.log('*****************SESSION CALLBACK!!')
+      //console.log('session', session)
+      //console.log('token', token)
+      //console.log('user', user)
+      session.accessToken = session.token.accessToken
+      session.refreshtokenToken = session.token.refreshToken;
+
+      return session;
+    },
+  },
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
---
+++ src/app/api/drive/route.ts
@@ -0,0 +1,113 @@
+import { google } from 'googleapis';
+import { getToken } from "next-auth/jwt"
+
+async function tokenDecode(req) {
+  try {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    if (token) {
+      //console.log('Decoded JWT:', token);
+      console.log('returning token', token.accessToken)
+      return token.accessToken
+    } else {
+      console.log('Invalid or expired JWT');
+    }
+  } catch (error) {
+    console.error('Error verifying JWT:', error);
+  }
+}
+
+export async function POST(
+  req: Request
+) {
+  try {
+
+    const accessToken = await tokenDecode(req)
+
+    const clientId = process.env.NEXTAUTH_CLIENT_ID;
+    const clientSecret = process.env.GOOGLE_SECRET;
+    const auth = new google.auth.OAuth2({
+      clientId, clientSecret,
+    })
+    auth.setCredentials({ access_token: accessToken })
+
+    const drive = google.drive({
+      version: 'v3',
+      auth: auth,
+    })
+
+    const { fileName, content } = await req.json();
+
+    const fileMetadata = {
+      'name': fileName,
+      'mimeType': 'application/json',
+      'parents': ['appDataFolder'],
+    }
+
+    const media = {
+      mimeType: 'application/json',
+      body: JSON.stringify(content)
+    }
+
+    const response = await drive.files.create({ resource: fileMetadata, media, fields: 'id' });
+
+    return new Response(JSON.stringify(response), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+  } catch (error) {
+    console.error('Google API returned an error:', error);
+    return new Response(JSON.stringify({ error: 'Google API error' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+  }
+}
+
+
+export async function GET(
+  req: Request
+) {
+  try {
+    const accessToken = await tokenDecode(req)
+
+    const clientId = process.env.NEXTAUTH_CLIENT_ID;
+    const clientSecret = process.env.GOOGLE_SECRET;
+
+    const auth = new google.auth.OAuth2({
+      clientId, clientSecret,
+    })
+    auth.setCredentials({ access_token: accessToken })
+
+    const drive = google.drive({
+      version: 'v3',
+      auth: auth,
+    })
+
+    // still does not work
+    //const { drive } = await connectGoogleDrive(req)
+
+    const response = await drive.files.list({
+      spaces: 'appDataFolder',
+    })
+    console.log(response.data)
+
+    return new Response(JSON.stringify(response.data), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+  } catch (error) {
+    console.error('Google API returned an error:', error);
+    return new Response(JSON.stringify({ error: 'Google API error' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+  }
+}
