9fd71ac0253b067da50005215236b662d4430e18
Fritz Meissner <fritz.meissner@gmail.com>
2023-12-22 15:26:51 +0000

    Add GET /drive/[id] endpoint
    Add a new endpoint that retrieves a file in Google Drive based on the id slug.

---
+++ src/app/api/drive/[id]/route.ts
@@ -0,0 +1,52 @@
+import { google } from 'googleapis';
+import { getToken } from "next-auth/jwt"
+
+async function tokenDecode(req) {
+  try {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    if (token) {
+      //console.log('Decoded JWT:', token);
+      console.log('returning token', token.accessToken)
+      return token.accessToken
+    } else {
+      console.log('Invalid or expired JWT');
+    }
+
+  } catch (error) {
+    console.error('Error verifying JWT:', error);
+  }
+}
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id
+
+  const accessToken = await tokenDecode(request)
+
+  const clientId = process.env.NEXTAUTH_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_SECRET;
+
+  const auth = new google.auth.OAuth2({
+    clientId, clientSecret,
+  })
+  auth.setCredentials({ access_token: accessToken })
+
+  const drive = google.drive({
+    version: 'v3',
+    auth: auth,
+  })
+
+  const lastFile = await drive.files.get({
+    spaces: 'appDataFolder',
+    fileId: id,
+    alt: 'media'
+  })
+  console.log('lastFile.data', lastFile.data)
+
+  return new Response(JSON.stringify(lastFile.data), {
+    status: 200,
+    headers: {'Content-Type': 'application/json'}
+  })
+}
