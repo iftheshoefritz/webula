84a7ca1b91c909b12d9487f8187f6ad085dce29e
Fritz Meissner <fritz.meissner@gmail.com>
2024-01-19 16:00:04 +0000

    Convert DeckUploader to tsx

---
+++ src/components/DeckUploader.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent } from 'react';
+
+interface DeckUploaderProps {
+  onFileLoad: (file: string) => void;
+}
+
+const DeckUploader: FunctionComponent<DeckUploaderProps> = ({ onFileLoad }) => {
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files[0];
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+      onFileLoad(e.target.result as string);
+    };
+
+    reader.onerror = (e) => {
+      console.error('File reading error', e);
+    };
+
+    reader.readAsText(file);
+  };
+
+  return (
+    <div>
+      <div>
+        <label htmlFor="fileInput" className="bg-black hover:bg-gray-600 text-white font-bold inline-block py-2 px-4 rounded cursor-pointer">
+          <input id="fileInput" type="file" onChange={handleFileUpload} className="hidden" />
+          Upload deck
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default DeckUploader;
