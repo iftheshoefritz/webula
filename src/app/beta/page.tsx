49653e192012bff0e2d03f917286855988f2c4be
Fritz Meissner <fritz.meissner@gmail.com>
2024-08-28 19:43:15 +0100

    Only show 2 cols in search results on large or bigger displays

--- src/app/beta/page.tsx
+++ src/app/beta/page.tsx
@@ -318,7 +318,7 @@ export default function Home() {
                 &lt;&lt; Back to list
                   </button>
                   <Help/>
-                  <div className="grid grid-cols-2 gap-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
                     <SearchResults filteredData={filteredData} onCardSelected={incrementIncluded} onCardDeselected={decrementIncluded} currentDeck={currentDeck} withHover={true}/>
                   </div>
                 </div>
